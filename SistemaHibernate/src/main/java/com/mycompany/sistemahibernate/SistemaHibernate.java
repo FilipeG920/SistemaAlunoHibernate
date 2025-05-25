d35d118ea1bbe24060a43a091eb0b1c72c8d2bc7
FilipeG920 <genesisfilipe11@gmail.com>
2025-05-25 19:25:45 -0300

    feat: classe main do sistema criada

---
+++ SistemaHibernate/src/main/java/com/mycompany/sistemahibernate/SistemaHibernate.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemahibernate;
+
+/**
+ *
+ * @author Filipe
+ */
+public class SistemaHibernate {
+
+    public static void main(String[] args) {
+        
+        FormularioGUI form = new FormularioGUI();
+        System.out.println("Hello World!");
+    }
+}
